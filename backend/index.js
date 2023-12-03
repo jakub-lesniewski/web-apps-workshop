5f0dc7ad12d788a297531be69082b4b3d47167d9
ayroh <jakubjanlesniewski@gmail.com>
2023-12-03 21:33:32 +0100

    added in call timer

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const config = {
-  url: "https://uni-call.fcc-online.pl",
-  login: "focus04",
-  password: "#dj6umytbhns",
+    url: "https://uni-call.fcc-online.pl",
+    login: "focus04",
+    password: "#dj6umytbhns",
 };
 
 dialer.configure(config);
@@ -19,68 +19,78 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
 });
 
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}`);
-// });
-
 const serverInstance = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+    console.log(`App running on port ${port}`);
 });
 
 const io = new Server(serverInstance);
 
 app.get("/test", (req, res) => {
-  res.json({ message: "test" });
+    res.json({ message: "test" });
 });
 
 app.get("/call/:number1/:number2", async (req, res) => {
-  const number1 = req.params.number1;
-  const number2 = req.params.number2;
+    const number1 = req.params.number1;
+    const number2 = req.params.number2;
 
-  try {
-    await dialer.call(number1, number2);
-    res.json({ success: true });
-  } catch (error) {
-    console.error("Error making the call:", error);
-    res.status(500).json({ success: false, error: "Failed to make the call" });
-  }
+    try {
+        await dialer.call(number1, number2);
+        res.json({ success: true });
+    } catch (error) {
+        console.error("Error making the call:", error);
+        res.status(500).json({
+            success: false,
+            error: "Failed to make the call",
+        });
+    }
 });
 
 app.post("/call/", async (req, res) => {
-  const number1 = req.body.number;
-  const number2 = "573055986";
+    try {
+        const number1 = req.body.number;
+        const number2 = "573055986";
 
-  console.log("Calling", number1, number2);
+        console.log("Calling", number1, number2);
 
-  const bridge = await dialer.call(number1, number2);
+        const bridge = await dialer.call(number1, number2);
 
-  let oldStatus = null;
+        let oldStatus = null;
 
-  let interval = setInterval(async () => {
-    let currentStatus = await bridge.getStatus();
-    if (currentStatus !== oldStatus) {
-      oldStatus = currentStatus;
-      io.emit("status", currentStatus);
+        let interval = setInterval(async () => {
+            let currentStatus = await bridge.getStatus();
+            if (currentStatus !== oldStatus) {
+                oldStatus = currentStatus;
+                io.emit("status", currentStatus);
+            }
+            if (
+                currentStatus === "ANSWERED" ||
+                currentStatus === "FAILED" ||
+                currentStatus === "BUSY" ||
+                currentStatus === "NO ANSWER"
+            ) {
+                console.log("stop");
+                clearInterval(interval);
+            }
+        }, 1000);
+
+        res.json({ id: "123", status: bridge.STATUSES.NEW });
+    } catch (error) {
+        console.error("An error occurred:", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
-    if (
-      currentStatus === "ANSWERED" ||
-      currentStatus === "FAILED" ||
-      currentStatus === "BUSY" ||
-      currentStatus === "NO ANSWER"
-    ) {
-      console.log("stop");
-      clearInterval(interval);
-    }
-  }, 1000);
-  res.json({ id: "123", status: bridge.STATUSES.NEW });
 });
 
 app.get("/status", async function (req, res) {
-  let status = await bridge.getStatus();
-  res.json({ id: "123", status: status });
+    try {
+        let status = await bridge.getStatus();
+        res.json({ id: "123", status: status });
+    } catch (error) {
+        console.error("An error occurred:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
